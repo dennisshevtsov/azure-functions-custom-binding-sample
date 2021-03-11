f545836ea811f52f6607b28fe647e734f4a2c158
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-03-11 13:11:10 +0300

    get task

--- AzureFunctionsCustomBindingSample.Api/Dtos/GetTodoListRequestDto.cs
+++ AzureFunctionsCustomBindingSample.Api/Dtos/GetTodoListRequestDto.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
 // Licensed under the Apache License, Version 2.0.
 // See License.txt in the project root for license information.
-
 namespace AzureFunctionsCustomBindingSample.Api.Dtos
 {
+  using System;
+
   /// <summary>Represents conditions to query a TODO list.</summary>
   public sealed class GetTodoListRequestDto
   {
+    /// <summary>Gets/sets a value that represents an ID of a TODO list.</summary>
+    public Guid TodoListId { get; set; }
   }
 }
--- AzureFunctionsCustomBindingSample.Api/Dtos/GetTodoListResponseDto.cs
+++ AzureFunctionsCustomBindingSample.Api/Dtos/GetTodoListResponseDto.cs
@@ -4,8 +4,29 @@
 
 namespace AzureFunctionsCustomBindingSample.Api.Dtos
 {
+  using System;
+  using System.Collections.Generic;
+
   /// <summary>Represents detail of a TODO list.</summary>
   public sealed class GetTodoListResponseDto
   {
+    /// <summary>Gets/sets a value that represents a title of a TODO list.</summary>
+    public string Title { get; set; }
+
+    /// <summary>Gets/sets a value that represents a description of a TODO list.</summary>
+    public string Description { get; set; }
+
+    /// <summary>Gets/sets an object that represents a collection of TODO list tasks.</summary>
+    public IEnumerable<GetTodoListTaskResponseDto> Tasks { get; set; }
+
+    /// <summary>Represents detail of a TODO list task.</summary>
+    public sealed class GetTodoListTaskResponseDto
+    {
+      /// <summary>Gets/sets a value that represents an ID of a TODO list task.</summary>
+      public Guid TodoListTaskId { get; set; }
+
+      /// <summary>Gets/sets a value that represents a title of a TODO list task.</summary>
+      public string Title { get; set; }
+    }
   }
 }
