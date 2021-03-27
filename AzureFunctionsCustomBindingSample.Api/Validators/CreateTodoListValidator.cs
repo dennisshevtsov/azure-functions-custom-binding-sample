adc9150c3f562b501df8761583652005991c457e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-03-27 12:38:45 +0300

    comments

--- AzureFunctionsCustomBindingSample.Api/Validators/CreateTodoListValidator.cs
+++ AzureFunctionsCustomBindingSample.Api/Validators/CreateTodoListValidator.cs
@@ -10,13 +10,18 @@ namespace AzureFunctionsCustomBindingSample.Api.Validators
   using AzureFunctionsCustomBindingSample.Api.Dtos;
   using AzureFunctionsCustomBindingSample.Binding.Validation;
 
+  /// <summary>Provides a simple API to validate an HTTP request.</summary>
   public sealed class CreateTodoListValidator : IValidator
   {
     private readonly CreateTodoListRequestDto _requestDto;
 
+    /// <summary>Initializes a new instance of the <see cref="AzureFunctionsCustomBindingSample.Api.Validators."/> class.</summary>
+    /// <param name="requestDto">An object that represents data to create a TODO list.</param>
     public CreateTodoListValidator(CreateTodoListRequestDto requestDto)
       => _requestDto = requestDto ?? throw new ArgumentNullException(nameof(requestDto));
 
+    /// <summary>Validates an HTTP request.</summary>
+    /// <returns>An object that represents a collection of errors.</returns>
     public IEnumerable<string> Validate()
     {
       if (string.IsNullOrWhiteSpace(_requestDto.Title))
--- AzureFunctionsCustomBindingSample.Api/Validators/UpdateTodoListTaskValidator.cs
+++ AzureFunctionsCustomBindingSample.Api/Validators/UpdateTodoListTaskValidator.cs
@@ -10,13 +10,18 @@ namespace AzureFunctionsCustomBindingSample.Api.Validators
   using AzureFunctionsCustomBindingSample.Api.Dtos;
   using AzureFunctionsCustomBindingSample.Binding.Validation;
 
+  /// <summary>Provides a simple API to validate an HTTP request.</summary>
   public sealed class UpdateTodoListTaskValidator : IValidator
   {
     private readonly UpdateTodoListTaskRequestDto _requestDto;
 
+    /// <summary>Initializes a new instance of the <see cref="AzureFunctionsCustomBindingSample.Api.Validators."/> class.</summary>
+    /// <param name="requestDto">An object that represents data to a task of a TODO list.</param>
     public UpdateTodoListTaskValidator(UpdateTodoListTaskRequestDto requestDto)
       => _requestDto = requestDto ?? throw new ArgumentNullException(nameof(requestDto));
 
+    /// <summary>Validates an HTTP request.</summary>
+    /// <returns>An object that represents a collection of errors.</returns>
     public IEnumerable<string> Validate()
     {
       if (string.IsNullOrWhiteSpace(_requestDto.Title))
