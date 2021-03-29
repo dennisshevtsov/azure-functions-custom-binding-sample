3505d6aa5895d91ed73ef83905eeb1d32eb9ba79
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-03-29 11:48:16 +0300

    refactoring

--- AzureFunctionsCustomBindingSample.Api/QueryHandlers/CompleteTodoListTaskQueryHandler.cs
+++ AzureFunctionsCustomBindingSample.Api/QueryHandlers/CompleteTodoListTaskQueryHandler.cs
@@ -1,15 +1,18 @@
-﻿using AzureFunctionsCustomBindingSample.Api.Documents;
-using AzureFunctionsCustomBindingSample.Api.Dtos;
-using AzureFunctionsCustomBindingSample.Binding.Document;
-using AzureFunctionsCustomBindingSample.CosmosDb;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
 
 namespace AzureFunctionsCustomBindingSample.Api.QueryHandlers
 {
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  using AzureFunctionsCustomBindingSample.Api.Documents;
+  using AzureFunctionsCustomBindingSample.Api.Dtos;
+  using AzureFunctionsCustomBindingSample.Binding.Document;
+  using AzureFunctionsCustomBindingSample.CosmosDb;
+
   public sealed class CompleteTodoListTaskQueryHandler : IQueryHandler<CompleteTodoListTaskRequestDto>
   {
     private readonly IDocumentClient _documentClient;
