b003cad917866b1de58f3c8cb61a075e8289aebc
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-03-29 11:49:10 +0300

    refactoring

--- AzureFunctionsCustomBindingSample.Binding/Document/IQueryHandler.cs
+++ AzureFunctionsCustomBindingSample.Binding/Document/IQueryHandler.cs
@@ -1,8 +1,12 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
 
 namespace AzureFunctionsCustomBindingSample.Binding.Document
 {
+  using System.Threading;
+  using System.Threading.Tasks;
+
   public interface IQueryHandler<TQuery>
   {
     public Task<object> HandleAsync(TQuery query, CancellationToken cancellationToken);
