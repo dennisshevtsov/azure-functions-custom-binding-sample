34440e99e9aac14c94da4185f17fbe3b6c1e0254
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-05 13:54:39 +0300

    start query resolving

---
+++ AzureFunctionsCustomBindingSample.Binding/Document/IQueryResolver.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+namespace AzureFunctionsCustomBindingSample.Binding.Document
+{
+  using System.Collections.Generic;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  using Microsoft.AspNetCore.Http;
+
+  public interface IQueryResolver
+  {
+    public Task<TDocument> ResolveOneAsync<TDocument>(HttpRequest httpRequest, CancellationToken cancellationToken);
+
+    public Task<IEnumerable<TDocument>> ResolveManyAsync<TDocument>(HttpRequest httpRequest, CancellationToken cancellationToken);
+  }
+}
