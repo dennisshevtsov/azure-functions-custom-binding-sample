086d984a15b60f616d74a5e6d45928922fc26f53
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-26 13:27:16 +0300

    change license

--- AzureFunctionsCustomBindingSample.Binding.Tests/Authorization/AuthorizationValueProviderTest.cs
+++ AzureFunctionsCustomBindingSample.Binding.Tests/Authorization/AuthorizationValueProviderTest.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
-// Licensed under the Apache License, Version 2.0.
+// Licensed under the MIT License.
 // See License.txt in the project root for license information.
 
 namespace AzureFunctionsCustomBindingSample.Binding.Authorization.Tests
