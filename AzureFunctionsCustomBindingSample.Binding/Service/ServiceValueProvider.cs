e6561eaa67e7fa58dae13ae45535d4cc14b2854a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-28 01:44:19 +0300

    update license header

--- AzureFunctionsCustomBindingSample.Binding/Service/ServiceValueProvider.cs
+++ AzureFunctionsCustomBindingSample.Binding/Service/ServiceValueProvider.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
 // Licensed under the MIT License.
-// See License.txt in the project root for license information.
+// See LICENSE in the project root for license information.
 
 namespace AzureFunctionsCustomBindingSample.Binding.Service
 {
