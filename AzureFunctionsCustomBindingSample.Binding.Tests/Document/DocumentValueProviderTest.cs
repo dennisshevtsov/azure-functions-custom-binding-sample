75985f28c9ced4589ae7142bec59f7f443aa4764
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-05-06 00:47:20 +0300

    document binding test

--- AzureFunctionsCustomBindingSample.Binding.Tests/Document/DocumentValueProviderTest.cs
+++ AzureFunctionsCustomBindingSample.Binding.Tests/Document/DocumentValueProviderTest.cs
@@ -6,6 +6,7 @@ namespace AzureFunctionsCustomBindingSample.Binding.Document.Tests
 {
   using System;
   using System.Collections.Generic;
+  using System.Linq;
   using System.Threading;
   using System.Threading.Tasks;
 
@@ -26,25 +27,6 @@ namespace AzureFunctionsCustomBindingSample.Binding.Document.Tests
     public void Initialize()
     {
       _documentProviderMock = new Mock<IDocumentProvider>();
-      _documentProviderMock.Setup(provider => provider.GetDocumentAsync(It.IsAny<HttpRequest>(), It.IsAny<Type>(), It.IsAny<CancellationToken>()))
-                           .ReturnsAsync((HttpRequest httpRequest, Type type, CancellationToken CancellationToken) =>
-                           {
-                             object instance = null;
-
-                             if (httpRequest.HttpContext.Items.TryGetValue(
-                                   "__request__", out var requestObject) &&
-                                 requestObject is GetTestDocumentRequestDto requestDto)
-                             {
-                               instance = new TestDocument
-                               {
-                                 Id = requestDto.TestDocumentId,
-                               };
-                             }
-
-                             return instance;
-                           });
-      _documentProviderMock.Setup(provider => provider.GetDocumentsAsync(It.IsAny<HttpRequest>(), It.IsAny<Type>(), It.IsAny<CancellationToken>()))
-                           .ReturnsAsync((HttpRequest httpRequest, Type type, CancellationToken CancellationToken) => new[] { new TestDocument(), });
 
       _serviceProviderMock = new Mock<IServiceProvider>();
       _serviceProviderMock.Setup(provider => provider.GetService(It.IsAny<Type>()))
@@ -57,8 +39,6 @@ namespace AzureFunctionsCustomBindingSample.Binding.Document.Tests
       _httpRequestMock = new Mock<HttpRequest>();
       _httpRequestMock.SetupGet(request => request.HttpContext)
                       .Returns(_httpContextMock.Object);
-
-      _valueProvider = new DocumentValueProvider(typeof(TestDocument), _httpRequestMock.Object);
     }
 
     [TestMethod]
@@ -78,6 +58,30 @@ namespace AzureFunctionsCustomBindingSample.Binding.Document.Tests
       Assert.AreEqual(testId, testDocument.Id);
     }
 
+    [TestMethod]
+    public async Task GetValueAsync_Should_Return_Documents()
+    {
+      var stringProperty = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 5).ToUpper();
+      var dateTimeProperty = DateTime.UtcNow;
+
+      Setup(stringProperty, dateTimeProperty);
+
+      var value = await _valueProvider.GetValueAsync();
+
+      Assert.IsNotNull(value);
+
+      var testDocuments = value as IEnumerable<TestDocument>;
+
+      Assert.IsNotNull(testDocuments);
+
+      foreach (var testDocument in testDocuments)
+      {
+        Assert.IsNotNull(testDocument);
+        Assert.AreEqual(stringProperty, testDocument.StringProperty);
+        Assert.AreEqual(dateTimeProperty, testDocument.DateTimeProperty);
+      }
+    }
+
     private void Setup(Guid testId)
     {
       var items = new Dictionary<object, object>();
@@ -90,6 +94,59 @@ namespace AzureFunctionsCustomBindingSample.Binding.Document.Tests
 
       _httpContextMock.SetupGet(context => context.Items)
                       .Returns(items);
+      _valueProvider = new DocumentValueProvider(typeof(TestDocument), _httpRequestMock.Object);
+      _documentProviderMock.Setup(provider => provider.GetDocumentAsync(It.IsAny<HttpRequest>(), It.IsAny<Type>(), It.IsAny<CancellationToken>()))
+                           .ReturnsAsync((HttpRequest httpRequest, Type type, CancellationToken CancellationToken) =>
+                           {
+                             object instance = null;
+
+                             if (httpRequest.HttpContext.Items.TryGetValue(
+                                   "__request__", out var requestObject) &&
+                                 requestObject is GetTestDocumentRequestDto requestDto)
+                             {
+                               instance = new TestDocument
+                               {
+                                 Id = requestDto.TestDocumentId,
+                               };
+                             }
+
+                             return instance;
+                           });
+    }
+
+    private void Setup(string stringProperty, DateTime dateTimeProperty)
+    {
+      var items = new Dictionary<object, object>();
+
+      var requestDto = new SearchTestDocumentsRequestDto
+      {
+        StringProperty = stringProperty,
+        DateTimeProperty = dateTimeProperty,
+      };
+      items.Add("__request__", requestDto);
+
+      _httpContextMock.SetupGet(context => context.Items)
+                      .Returns(items);
+      _valueProvider = new DocumentValueProvider(typeof(IEnumerable<TestDocument>), _httpRequestMock.Object);
+      _documentProviderMock.Setup(provider => provider.GetDocumentsAsync(It.IsAny<HttpRequest>(), It.IsAny<Type>(), It.IsAny<CancellationToken>()))
+                           .ReturnsAsync((HttpRequest httpRequest, Type type, CancellationToken CancellationToken) =>
+                           {
+                             object instance = null;
+
+                             if (httpRequest.HttpContext.Items.TryGetValue(
+                                   "__request__", out var requestObject) &&
+                                 requestObject is SearchTestDocumentsRequestDto requestDto)
+                             {
+                               instance = Enumerable.Range(1, 5)
+                                                    .Select(number => new TestDocument
+                                                    {
+                                                      StringProperty = requestDto.StringProperty,
+                                                      DateTimeProperty = requestDto.DateTimeProperty,
+                                                    });
+                             }
+
+                             return instance;
+                           });
     }
   }
 }
